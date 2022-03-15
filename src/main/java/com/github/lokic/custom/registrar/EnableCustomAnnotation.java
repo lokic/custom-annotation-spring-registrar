87657e9f406fd2612362d74f0e8b0b13a55e3090
loki <kiraconan2046@gmail.com>
2022-03-15 22:17:47 +0800

    docs: 增加部分注释

--- src/main/java/com/github/lokic/custom/registrar/EnableCustomAnnotation.java
+++ src/main/java/com/github/lokic/custom/registrar/EnableCustomAnnotation.java
@@ -7,7 +7,9 @@ import java.lang.annotation.*;
 /**
  * 默认的EnableXxx的Annotation，适合本地项目中快捷使用custom annotation的场景。
  * <p>
- * <p>建议使用自定义EnableXxx，能更加便捷得扩展二方包或者三方包。如
+ * <p>建议使用自定义EnableXxx，能更加便捷得扩展二方包或者三方包。
+ * <p>Note: 如果使用自定义EnableXxx，需要覆盖 {@link com.github.lokic.custom.registrar.InterfaceRegistrar#getEnableAnnotationType}
+ * <p>自定义EnableXxx参考实现如下：
  * <pre class="code">
  * &#064;Retention(RetentionPolicy.RUNTIME)
  * &#064;Target(ElementType.TYPE)
@@ -17,11 +19,12 @@ import java.lang.annotation.*;
  *     String[] basePackages() default {};
  * }
  * </pre>
+ *
  */
-@Import(CustomAnnotationSelector.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Documented
+@Import(CustomAnnotationSelector.class)
 public @interface EnableCustomAnnotation {
 
     Class<? extends InterfaceRegistrar>[] registrarTypes();
